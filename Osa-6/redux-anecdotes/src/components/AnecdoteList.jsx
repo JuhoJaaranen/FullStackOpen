1ab3e600c0533ac9138a58a31153e76b7d77f529
JuhoJaaranen <juho.jaaranen@gmail.com>
2025-05-05 11:08:57 +0300

    6.9 valmis

--- Osa-6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Osa-6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,12 @@ import { useSelector, useDispatch } from "react-redux";
 import { handleLike } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    if ( state.filter !== 'ALL' ) {
+      return state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+    }
+    return state.anecdotes
+  })
   const dispatch = useDispatch();
   anecdotes.sort((a, b) => a.votes > b.votes ? -1 : 0);
 
