16f983a3a06844efe9b9f1057d9c387b7946fa61
JuhoJaaranen <juho.jaaranen@gmail.com>
2025-05-27 13:59:50 +0300

    Osa-6 valmis

--- Osa-6/query-anecdotes/src/App.jsx
+++ Osa-6/query-anecdotes/src/App.jsx
@@ -1,19 +1,45 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
+import { useNotificationDispatch } from './NotificationContext'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const dispatch = useNotificationDispatch()
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+    dispatch({ type: 'SET_NOTIF', payload: `Voted for ${anecdote.content}` })
+    setTimeout(() => {
+      dispatch({ type: 'HIDE_NOTIF' })
+    }, 5000)
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  if ( result.isError ) {
+    return <div>error...</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
--- Osa-6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ Osa-6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,30 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+import { useNotificationDispatch } from '../NotificationContext'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const dispatch = useNotificationDispatch()
+
+  const newAnecdoteMutation = useMutation({ 
+    mutationFn: createAnecdote,
+    onError: () => {
+      dispatch({ type: 'SET_NOTIF', payload: `too short anecdote, must have length 5 or more` })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+    dispatch({ type: 'SET_NOTIF', payload: `Created ${content}` })
+    setTimeout(() => {
+      dispatch({ type: 'HIDE_NOTIF' })
+    }, 5000)
 }
 
   return (
